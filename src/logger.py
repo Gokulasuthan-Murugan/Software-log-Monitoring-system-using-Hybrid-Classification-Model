aa33f8a0524627aab07fc2328534fcc7f3028f59
Gokulasuthan <gokulasuthan31@gmail.com>
2025-04-13 18:27:39 +0530

    updated logger.py

--- src/logger.py
+++ src/logger.py
@@ -2,14 +2,27 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m-%d-%Y-%H-%M-%S')}.log"
 logs_path=os.path.join(os.getcwd(),'logs',LOG_FILE)
-os.makedir(logs_path,exist_ok=True)
+os.makedirs(logs_path,exist_ok=True)
 
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="[%(asctime)s]  %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
+
+if __name__=="__main__":
+    logging.info("initialized logging file looger.py")
+
+
+"""
+%(asctime)s==> returns the time in readable format
+%(lineno)d==> Source line number where the logging call was issued (if available).
+%(name)s==> Name of the logger used to log the call.
+%(levelname)s==>returns the level like info,warnings,debug...
+%(message)s==>Actual log messsge 
+
+"""
