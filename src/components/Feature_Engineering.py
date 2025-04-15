6920acd09d7bae673142399c045b8e7c9cc206dc
Gokulasuthan <gokulasuthan31@gmail.com>
2025-04-15 11:03:04 +0530

    Feature_Engineering completed

---
+++ src/components/Feature_Engineering.py
@@ -0,0 +1,36 @@
+import os 
+import sys 
+import pickle 
+
+from sentence_transformers import SentenceTransformer
+from src.exception import CustomException
+from src.logger import logging
+from dataclasses import dataclass 
+
+@dataclass
+class FeatureEngineeringConfig:
+    Column_Transformer_path:str=os.path.join('Saved_Models','Column_Transformer.pkl')
+
+class FeatureEngineering:
+    def __init__(self):
+        self.feature_engineering_config=FeatureEngineeringConfig()
+    
+    def initiate_feature_engineering(self):
+        logging.info('FeatureEngineering initiated')
+        try:
+            os.makedirs(os.path.dirname(self.feature_engineering_config.Column_Transformer_path),exist_ok=True)
+            logging.info('Directory created')
+            model_encoder=SentenceTransformer('all-MiniLM-L6-v2')
+            with open(self.feature_engineering_config.Column_Transformer_path,'wb') as f:
+                pickle.dump(model_encoder,f)
+            logging.info(f'Model SentenceTransformer saved successfully in {self.feature_engineering_config.Column_Transformer_path}')
+            return self.feature_engineering_config.Column_Transformer_path
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+if __name__=="__main__":
+    obj=FeatureEngineering()
+    obj.initiate_feature_engineering()
+
+
--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -18,9 +18,7 @@ class DataIngestionConfig:
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
-        logging.info(f"Saving raw data to: {self.ingestion_config.raw_data_path}")
-        logging.info(f"Saving train data to: {self.ingestion_config.train_data_path}")
-        logging.info(f"Saving test data to: {self.ingestion_config.test_data_path}")
+
     def initiate_data_ingestion(self):
         logging.info('Method Data Ingestion')
         try:
@@ -29,11 +27,14 @@ class DataIngestion:
             
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True) #To make artifacts folder
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            logging.info(f"Saving raw data to: {self.ingestion_config.raw_data_path}")
             logging.info("Train test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
             logging.info("Train test split initicompleted")
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)    
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)    
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            logging.info(f"Saving train data to: {self.ingestion_config.train_data_path}") 
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info(f"Saving test data to: {self.ingestion_config.test_data_path}")   
             logging.info("Ingestion of data is completed successfully")
             return self.ingestion_config.train_data_path,self.ingestion_config.test_data_path
         except Exception as e:
