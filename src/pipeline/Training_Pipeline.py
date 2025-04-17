f49f99063dc59c5ce213aafde483588eee2cafea
Gokulasuthan <gokulasuthan31@gmail.com>
2025-04-17 10:31:07 +0530

    updated predict_pipeline and Training Pipeline

--- src/pipeline/Training_Pipeline.py
+++ src/pipeline/Training_Pipeline.py
@@ -21,18 +21,21 @@ class Training_Pipeline:
         
     
     def training_pipeline(self):
-        df=pd.read_csv(self.data_ingestion.initiate_data_ingestion(r'E:\Software-log-Monitoring-system-using-Hybrid-Classification-Model\notebook\synthetic_logs.csv'))
-        clustered_data_path,Clustered_model_path=self.clustering.initiate_clustering(r'E:\Software-log-Monitoring-system-using-Hybrid-Classification-Model\notebook\synthetic_logs.csv')
-        df_cluster=pd.read_csv(clustered_data_path)
-        embeddings_model_path=self.feature_engineering.initiate_feature_engineering()
-        with open(embeddings_model_path,'rb') as f:
-            embedding_model=pickle.load(f)
+        try:
+            df=pd.read_csv(self.data_ingestion.initiate_data_ingestion(r'E:\Software-log-Monitoring-system-using-Hybrid-Classification-Model\notebook\synthetic_logs.csv'))
+            clustered_data_path,Clustered_model_path=self.clustering.initiate_clustering(r'E:\Software-log-Monitoring-system-using-Hybrid-Classification-Model\notebook\synthetic_logs.csv')
+            df_cluster=pd.read_csv(clustered_data_path)
+            embeddings_model_path=self.feature_engineering.initiate_feature_engineering()
+            with open(embeddings_model_path,'rb') as f:
+                embedding_model=pickle.load(f)
 
-        self.logistic_model_path=self.logistic_model.train_logistic_model(clustered_data_path)
-        with open(self.logistic_model_path,'rb') as f:
-            Logistic_Model=pickle.load(f)
+            self.logistic_model_path=self.logistic_model.train_logistic_model(clustered_data_path)
+            with open(self.logistic_model_path,'rb') as f:
+                Logistic_Model=pickle.load(f)
 
-        return Logistic_Model
+            return Logistic_Model
+        except Exception as e:
+            raise CustomException(e,sys)
 
         
         
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -4,6 +4,7 @@ import sys
 from src.pipeline.Training_Pipeline import Training_Pipeline
 from src.components.Regex_Matcher import Regular_Expression
 from src.components.LLM_Model import LLMClassifier
+from src.components.Logistic_Model import LogisticModel
 from src.exception import CustomException
 from src.logger import logging
 
@@ -12,6 +13,17 @@ class PredictionPipeline:
         self.regex_matcher=Regular_Expression()
         self.LLM_classifier=LLMClassifier()
         self.Logistic_model=Training_Pipeline()
+        self.logisticmodel=LogisticModel()
     
-    def prediction_pipeline(self):
-        pass
+    def prediction_pipeline(self,message):
+        if self.regex_matcher.classify_regex(message)!=None:
+            print(self.regex_matcher.classify_regex(message))
+        elif self.logisticmodel.Classify_log_model(message) !="unknown":
+            print(self.logisticmodel.Classify_log_model(message))
+        else:
+            print(self.LLM_classifier.classify_log_message(message))
+        
+
+         
+
+        
